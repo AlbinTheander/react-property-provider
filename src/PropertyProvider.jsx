9116d9ee3a98b0cfcdf3d7986ff415f5db46154b
Albin Theander <Albin.Theander@jayway.com>
2017-03-04 15:21:31 +0100

    Wrapped componets will update when properties change

--- src/PropertyProvider.jsx
+++ src/PropertyProvider.jsx
@@ -6,11 +6,18 @@ import PropertyHolder from './PropertyHolder';
 export default class PropertyProvider extends Component {
   constructor(props) {
     super(props);
+    const pureProps = { ...props };
+    delete pureProps.children;
     this.holder = new PropertyHolder(props);
   }
 
   getChildContext() {
     return { propertyHolder: this.holder };
+  }
+
+  componentWillReceiveProps(newProps) {
+    Object.keys(newProps).forEach(prop =>
+      prop !== 'children' && this.holder.set(prop, newProps[prop]));
   }
 
   render() {
--- src/__test__/with-properties.test.jsx
+++ src/__test__/with-properties.test.jsx
@@ -1,18 +1,71 @@
-import React from 'react';
+import React, { Children, Component } from 'react';
 import { mount } from 'enzyme';
 
 import PropertyProvider, { withProperties } from '..';
 
+/* eslint-disable react/prop-types */
+
+const Status = withProperties(({ status }) => <span>{status}</span>, 'status');
+const TwoTexts = withProperties(({ text1, text2 }) => <span>{text1} {text2}</span>, 'text1');
+
+class NeverUpdatingComponent extends Component {
+  shouldComponentUpdate() {
+    return false;
+  }
+  render() {
+    return Children.only(this.props.children);
+  }
+}
+
 describe('withProperty', () => {
   test('can access properties in the property provider', () => {
-    // eslint-disable-next-line react/prop-types
-    const TestComponent = ({ status }) => <span>{status}</span>;
-    const ProvidedComponent = withProperties(TestComponent, 'status');
     const wrapper = mount(
       <PropertyProvider status="cool">
-        <ProvidedComponent />
+        <Status />
       </PropertyProvider>);
 
     expect(wrapper).toHaveText('cool');
+  });
+
+  test('will update component when property changes', () => {
+    const wrapper = mount(
+      <PropertyProvider status="cool">
+        <Status />
+      </PropertyProvider>);
+
+    wrapper.setProps({ status: 'awesome' });
+    expect(wrapper).toHaveText('awesome');
+  });
+
+  test('will prioritize explicit properties', () => {
+    const wrapper = mount(
+      <PropertyProvider status="cool">
+        <Status status="awesome" />
+      </PropertyProvider>);
+
+    expect(wrapper).toHaveText('awesome');
+  });
+
+  test('will pass through other properties', () => {
+    const wrapper = mount(
+      <PropertyProvider text1="bacon">
+        <TwoTexts text2="cheese" />
+      </PropertyProvider>);
+
+    expect(wrapper).toHaveText('bacon cheese');
+  });
+
+  test('will update component even inside non-updating component', () => {
+    const wrapper = mount(
+      <PropertyProvider status="cool">
+        <NeverUpdatingComponent>
+          <Status />
+        </NeverUpdatingComponent>
+      </PropertyProvider>);
+
+    expect(wrapper).toHaveText('cool');
+
+    wrapper.setProps({ status: 'awesome' });
+    expect(wrapper).toHaveText('awesome');
   });
 });
